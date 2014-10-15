cb340edfb99cc16b110e0d5042dbe731920c2ff1
ppuri <ppuri@guidewire.com>
2014-10-15 15:23:33 -0700

    Fixing Utils.getLength()

--- src/Utils.java
+++ src/Utils.java
@@ -64,23 +64,33 @@ public class Utils {
   public static int getLength(int num) {
     if (num > 100000000) {
       return 9;
-    } else if (num > 10000000) {
+    } else if (num >= 10000000) {
       return 8;
-    } else if (num > 1000000) {
+    } else if (num >= 1000000) {
       return 7;
-    } else if (num > 100000) {
+    } else if (num >= 100000) {
       return 6;
-    } else if (num > 10000) {
+    } else if (num >= 10000) {
       return 5;
-    } else if (num > 1000) {
+    } else if (num >= 1000) {
       return 4;
-    } else if (num > 100) {
+    } else if (num >= 100) {
       return 3;
-    } else if (num > 10) {
+    } else if (num >= 10) {
       return 2;
     } else  {
       return 1;
     }
+  }
+
+  // get digits in the number
+  public static List<Integer> getDigits(int num)  {
+    List<Integer> li = new ArrayList<>();
+    while (num > 0) {
+      li.add(num%10);
+      num /= 10;
+    }
+    return li;
   }
 
 }
